f2566b90f0882914bf2b10903968799168e40ee1
kiran <kiranrajbimla@gmail.com>
2024-07-26 15:09:29 +0530

    part 5 deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,39 @@
+// // import express from "express";
+// // import dotenv from "dotenv";
+// // import cookiePaeser from "cookie-parser";
+
+// // import authRoutes from "./routes/auth.routes.js";
+// // import messageRoutes from "./routes/message.routes.js";
+// // import userRoutes from "./routes/user.routes.js";
+
+// // import connectToMongoDB from "./db/ConnectToMongoDB.js";
+// // import { app, server } from "./socket/socket.js";
+
+
+// // const PORT= process.env.PORT || 5000;
+
+// // dotenv.config();
+
+// // app.use(express.json());
+// // app.use(cookiePaeser());
+// // //to parse the incoming requests with json payloads
+// // app.use("/api/auth",authRoutes);
+// // app.use("/api/messages",messageRoutes);
+// // app.use("/api/users",userRoutes);
+
+// // // app.get("/",(req,res)=>{
+// // //     res.send("hello world kiran !");
+// // // });
+
+
+
+// // server.listen(PORT,()=>{
+// //     connectToMongoDB();
+// //     console.log(`server running on port ${PORT}`)
+// // });
 // import express from "express";
 // import dotenv from "dotenv";
-// import cookiePaeser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 
 // import authRoutes from "./routes/auth.routes.js";
 // import messageRoutes from "./routes/message.routes.js";
@@ -9,28 +42,26 @@
 // import connectToMongoDB from "./db/ConnectToMongoDB.js";
 // import { app, server } from "./socket/socket.js";
 
-
-// const PORT= process.env.PORT || 5000;
-
 // dotenv.config();
 
+// const PORT = process.env.PORT || 5000;
+
 // app.use(express.json());
-// app.use(cookiePaeser());
-// //to parse the incoming requests with json payloads
-// app.use("/api/auth",authRoutes);
-// app.use("/api/messages",messageRoutes);
-// app.use("/api/users",userRoutes);
+// app.use(cookieParser());
 
-// // app.get("/",(req,res)=>{
-// //     res.send("hello world kiran !");
-// // });
+// app.use("/api/auth", authRoutes);
+// app.use("/api/messages", messageRoutes);
+// app.use("/api/users", userRoutes);
 
-
-
-// server.listen(PORT,()=>{
+// server.listen(PORT, () => {
 //     connectToMongoDB();
-//     console.log(`server running on port ${PORT}`)
+//     console.log(`Server running on port ${PORT}`);
 // });
+
+
+
+
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -39,21 +70,29 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import connectToMongoDB from "./db/ConnectToMongoDB.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Server running on port ${PORT}`);
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
